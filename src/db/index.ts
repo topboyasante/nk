3e10f124019c20c0034ab3b596456c3a8df41c34
Nana K. <73991180+topboyasante@users.noreply.github.com>
2024-03-26 08:59:54 +0000

    ui

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,33 +1,41 @@
 export const projects = [
+  // {
+  //   id: 0,
+  //   title: "Snippets",
+  //   about:
+  //     "Web app to store code snippets. Built with ShadcnUI, NextJS + TS, and ShikiJS.",
+  //   link: "https://github.com/topboyasante/snippets",
+  //   link_text: "WIP - View GitHub",
+  // },
+  // {
+  //   id: 0,
+  //   title: "EasySell",
+  //   about: "Minimalist E-Commerce Solution for Very Small Businesses",
+  //   link: "https://github.com/topboyasante/easy-sell",
+  //   link_text: "WIP - View GitHub",
+  // },
   {
     id: 0,
-    title: "Snippets",
-    about:
-      "Web app to store code snippets. Built with ShadcnUI, NextJS + TS, and ShikiJS.",
-    link: "https://github.com/topboyasante/snippets",
-    link_text: "WIP - View GitHub",
-  },
-  {
-    id: 0,
-    title: "EasySell",
-    about: "Minimalist E-Commerce Solution for Very Small Businesses",
-    link: "https://github.com/topboyasante/easy-sell",
-    link_text: "WIP - View GitHub",
-  },
-  {
-    id: 1,
     title: "TLDR-Bot",
     about: "Integrating the TLDRThis API into a NextJS Application.",
     link: "https://tldr-ai.vercel.app/",
     link_text: "tldr-ai.vercel.app",
   },
   {
-    id: 2,
-    title: "Hive",
-    about: "A Task Outsourcing Platform built with NextJS and Rails.",
-    link: "https://github.com/topboyasante/hive-web",
-    link_text: "WIP - View GitHub",
+    id: 1,
+    title: "Issues.IO",
+    about:
+      "Dead Simple Issues Tracking System. Built with NextJS, Prisma, and Supabase.",
+    link: "https://github.com/topboyasante/issues.io",
+    link_text: "View Repo",
   },
+  // {
+  //   id: 2,
+  //   title: "Hive",
+  //   about: "A Task Outsourcing Platform built with NextJS and Rails.",
+  //   link: "https://github.com/topboyasante/hive-web",
+  //   link_text: "WIP - View GitHub",
+  // },
   {
     id: 3,
     title: "Ball Roll 3D",
